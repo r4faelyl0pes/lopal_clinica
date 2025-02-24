a1802dd3db4694d66fa73bc9381476e5744ecf3c
RAFAELY LOPES DE OLIVEIRA <25132819@CFP127ALUNO.LOCAL>
2025-02-24 17:02:22 -0300

    Começamos o primeiro gets e sets

--- src/br/senai/sp/jandira/clinica/ClinicaApp.java
+++ src/br/senai/sp/jandira/clinica/ClinicaApp.java
@@ -10,20 +10,20 @@ public class ClinicaApp {
 	
 		// Criar um objeto do tipo Paciente
 		Paciente p1 = new Paciente();
-		p1.nome = "Ana Maria";
+		p1.setNome("ana maria");
 		p1.cpf = "999.888.666-77";
-		p1.altura = 1.63;
-		p1.peso = 75.450;
+		p1.setAltura(11.63);
+		p1.setPeso(70.450);
 		p1.telefone = "(11)96523-8888";
 		p1.dataNascimento = LocalDate.of(2003,8, 16);
 				
 		
 		
 		Paciente p2 = new Paciente();
-		p2.nome = "Rafaely";
+		p2.setNome("rafaely");
 		p2.cpf = "444.555.666.77";
-		p2.altura = 1.45;
-		p2.peso = 35.550;
+		p2.setAltura(1.48);
+		p2.setPeso(35.550);
 		p2.telefone = "(11)99120-6849";
 		p2.dataNascimento = LocalDate.of(2006,11,29);
 		
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -4,24 +4,56 @@ import java.time.LocalDate;
 
 public class Paciente {
 
-	public String nome;
+	private String nome;
 	public String cpf;
-	public double altura;
-	public double peso;
+	private double altura;
+	private double peso;
 	public String telefone;
 	public LocalDate dataNascimento;
+
+	public void setAltura(double altura) {
+
+		if (altura > 0 && altura < 3) {
+			// TRUE
+			this.altura = altura;
+		} else {
+			// FALSE
+			System.out.println("A altura do(a) paciente " + nome + 
+					" deve ser maior que zero e menor que três!");
+		}
+
+	}
+
+	public void setPeso(double peso) {
+		
+		if (peso > 30 && peso < 500) {
+			// TRUE
+			this.peso = peso;
+		} else {
+			// FALSE
+		System.out.println("O peso do(a) paciente " + nome + 
+				" deve ser maior que trinta e menor que quinhentos!");
+		}
+	}
 	
-	public void mostrarDados () {
+	public void setNome (String nome) {
+		this.nome = nome.toUpperCase();
+	}
+
+	public String getNome() {
+		return nome; 
+	}
+
+	public void mostrarDados() {
 		System.out.println("----------------------------------------");
 		System.out.println("DADOS DO PACIENTE");
 		System.out.println("----------------------------------------");
 		System.out.println("Nome: " + nome);
 		System.out.println("Peso: " + peso + " kgs");
 		System.out.println("Altura: " + altura + "m");
-		System.out.println("Cpf: "+ cpf);
+		System.out.println("Cpf: " + cpf);
 		System.out.println("----------------------------------------");
 		System.out.println();
 	}
 
 }
-
